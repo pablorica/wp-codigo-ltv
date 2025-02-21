971ef9d811e7779715f3ed6dc2f5fb33239c853b
Pablo Rica <pabloricaaldeguer@gmail.com>
2025-02-21 10:58:18 +0100

    Last project: Canvas Carrers

--- bud.config.js
+++ bud.config.js
@@ -42,7 +42,7 @@ export default async (app) => {
 */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('https://sparemytime.localhost')
+    .setProxyUrl('https://canvascareers.localhost')
     .watch([
         `resources/views`,
         `resources/scripts`,
